736ba172a814883bd486d3316d35cf1a86cbdfa6
luizguilhermecm <luizgui@gmail.com>
2013-07-31 23:51:05 -0300

    pegando lista de todas as chamadas por ano passado por parametro

--- raw.rb
+++ raw.rb
@@ -37,12 +37,16 @@ def GetNames(page_url)
         vet.delete_at(1)
 
         current_key = "snk"
-
         vet.each_with_index do |x, index|
                 if x[0] != " " && x.length > 5
                         current_key = vet[index]
-                        @hash_uel[current_key] = []
+                        if @hash_uel.has_key?(x)
+                        
+                        else
+                                @hash_uel[current_key] = []
+                        end
                 end
+
                 if x.length > 10 
                         if x != current_key
                                 @hash_uel[current_key] << x.lstrip
@@ -62,8 +66,44 @@ def PrintHash (hash_to_print)
         end
 end
 
+def GetHashSize (aux)
+        puts "keys => " + aux.size.to_s
+        total = 0
+        aux.each do |key, value|
+#                puts "----------------------------------------------"
+#                puts "key => " + key.to_s 
+#                puts "----size => " + value.size.to_s
+                total += value.size
+        end
+        puts "====================="
+        puts total.to_s
+        puts "====================="
+end
+
+def GetByYear (ano)
+
+url_base_ini = 'http://www.cops.uel.br/vestibular/'
+
+url_base_meio = '/RESULTADO/2A_FASE/'
+
+url_base_fim = '/GERAL.TXT'
+
+goal = url_base_ini + ano.to_s + url_base_meio
+
+for i in 1..20
+        aux = goal + i.to_s + url_base_fim
+        begin
+                page = Nokogiri::HTML(open(aux))
+                GetNames(aux)                
+        rescue
+
+        end
+end
+
+end
 
 cops_uel_url = 'http://www.cops.uel.br/vestibular/2009/RESULTADO/2A_FASE/7/GERAL.TXT'
 
-GetNames(cops_uel_url)
+GetByYear(ARGV.first)
+GetHashSize(@hash_uel)
 PrintHash(@hash_uel)
